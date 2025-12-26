f18517599691edc9afef8f24cd90a5d4f71f239f
can2334 <101044119+can2334@users.noreply.github.com>
2025-12-26 11:54:25 +0300

    hatalar düzeltildi!

--- src/app/seyirci/page.tsx
+++ src/app/seyirci/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { gameApi, TeamStatus } from '../admin/services/api';
